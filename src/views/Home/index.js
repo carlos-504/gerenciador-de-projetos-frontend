3a00db3be50ada7236171c66ffc1f288769f0e81
Carlos Henrique <carllosshennriique@gmail.com>
2021-02-03 16:00:36 -0300

    Corpo da Home implementado

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import { ReactComponent as HomeSVG } from "../../assets/images/home.svg";
 import "./style.css";
 
 function Home() {
     return (
         <>
             <Navbar />
+            <div className="home">
+                <HomeSVG className="home-img" />
+                <h1>Bem-Vindo, [userName]</h1>
+
+                <div className="buttons">
+                    <button className="insert-btn">Cadastrar Projeto</button>
+                    <button className="list-btn">Listar Projetos</button>
+                </div>
+            </div>
         </>
     );
 }
